63f42d14e1b054728d881c08a649356c7e1092a5
sivaramanambikrishnan <157283782+sivaramanambikrishnan@users.noreply.github.com>
2025-06-01 08:34:49 +0530

    Update ChatBot.jsx

--- react-chatbot/src/ChatBot.jsx
+++ react-chatbot/src/ChatBot.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:5000'); 
+const socket = io('https://chat-ny4d.onrender.com'); 
 
 const ChatApp = () => {
   const [message, setMessage] = useState('');
